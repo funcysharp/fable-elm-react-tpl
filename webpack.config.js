bc4eb33045b3e4236a176a10912abfb61d79ff95
maestrow <andrew.shmelev@gmail.com>
2018-03-16 00:27:50 +0300

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,87 @@
-var path = require("path");
-var webpack = require("webpack");
-var fableUtils = require("fable-utils");
+const path = require("path");
+const fs = require("fs");
+const webpack = require("webpack");
+const fableUtils = require("fable-utils");
+const copyWebpackPlugin = require('copy-webpack-plugin');
 
 function resolve(filePath) {
   return path.join(__dirname, filePath)
 }
 
 var babelOptions = fableUtils.resolveBabelOptions({
-  presets: [
-    ["env", {
-      "targets": {
-        "browsers": "> 1%"
-      },
-      "modules": false
-    }]
-  ],
+  presets: [["es2015", { "modules": false }]],
+  plugins: ["transform-runtime"]
 });
+
+var out_path = resolve('./build');
 
 var isProduction = process.argv.indexOf("-p") >= 0;
 console.log("Bundling for " + (isProduction ? "production" : "development") + "...");
 
+var entry = isProduction 
+  ? { vendor: [ 
+        'react',
+        'react-dom',
+        'whatwg-fetch',
+      ],
+      main: "./src/app.fsproj" }
+  : resolve('./src/app.fsproj');
+
+var output = isProduction 
+  ? { publicPath: "/",
+      path: out_path,
+      filename: '[chunkhash].[name].js' }
+  : { filename: 'main.js',
+      path: out_path }
+
+var plugins = isProduction
+  ? [
+      new webpack.optimize.CommonsChunkPlugin({
+          names: ['vendor','manifest'] // Specify the common bundle names.
+      }),
+      new copyWebpackPlugin([
+          { from: 'src/index.html' }
+      ]),
+      function () {
+          this.plugin("done", function (stats) {
+              var replaceInFile = function (filePath, replacements) {
+                  var str = fs.readFileSync(filePath, 'utf8');
+                  replacements.forEach(function ({toReplace,replacement}) {
+                    var replacer = function (match) {
+                        console.log('Replacing in %s: %s => %s', filePath, match, replacement);
+                        return './' + replacement;
+                    };
+                    str = str.replace(new RegExp(toReplace, 'g'), replacer);
+                  });
+                  fs.writeFileSync(filePath, str);
+              };
+              var assetsByChunkName = stats.toJson().assetsByChunkName;
+              var regexStr = function (chunk) {
+                  return '\.\/([a-z0-9]*\.{0,1})' + chunk + '\.js';
+              }
+              replaceInFile(path.join(out_path, 'index.html'),
+                [ {toReplace: regexStr('main'), replacement: assetsByChunkName.main[0]},
+                  {toReplace: regexStr('manifest'), replacement: assetsByChunkName.manifest[0]},
+                  {toReplace: regexStr('vendor'), replacement: assetsByChunkName.vendor[0]} ]
+              );
+          });
+      }
+    ]
+  : [ new copyWebpackPlugin([
+          { from: 'src/index.html' }
+      ])];
+
 module.exports = {
   devtool: "source-map",
-  entry: resolve('./src/FableElmReactApp.fsproj'),
-  output: {
-    filename: 'bundle.js',
-    path: resolve('./public'),
-  },
+  entry: entry,
+  output: output,
   resolve: {
-    modules: [resolve("./node_modules/")]
+    modules: [
+      "node_modules", resolve("./node_modules/")
+    ]
   },
   devServer: {
-    contentBase: resolve('./public'),
+    contentBase: out_path,
     port: 8080
   },
   module: {
@@ -53,7 +103,16 @@ module.exports = {
           loader: 'babel-loader',
           options: babelOptions
         },
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
       }
     ]
-  }
+  },
+  plugins: plugins
 };
